28d802496bbd3f32503d84e6f322cb5a57053d9a
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-03-16 19:00:14 -0700

    Fix white space issue with emacs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description - Print "Programming is like building a multilingual puzzle with puts function
+ * Description - Print "Programming is like building
+ * a multilingual puzzle with puts function
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
