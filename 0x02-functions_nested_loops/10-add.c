e9c68e7d2d251640d8a1ea2caa7059a73e9a6719
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-03-22 21:49:52 +0000

    Add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: Addition of the integers.
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
