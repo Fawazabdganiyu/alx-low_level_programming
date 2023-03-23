2190fbd7db45195464a5666939df8642cde255cf
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-03-23 10:41:23 -0700

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: size of the square to print
+ *
+ * Return: The square.
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			if (size == size - 1)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+}
