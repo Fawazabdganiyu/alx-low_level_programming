753cc9a999ec1287773285a2d0664137a156bff1
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-03-28 17:20:43 +0000

    Initiate _strcpy()

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * _strcpy -  copies the string pointed to by src, including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: pointer to copy to
+ * @src: where the string is copied from
+ */
