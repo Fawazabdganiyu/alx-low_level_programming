010fe79e895d557cc266ff75515968910b815824
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-03-28 16:04:39 +0000

    Print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character
- *@str: input string
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: input string
  *
  */
 void puts2(char *str)
