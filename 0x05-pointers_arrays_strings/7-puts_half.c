3aba2f62af0d9efe1e8573ca0ce462cc63bdad6e
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-03-28 16:13:01 +0000

    Print second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str: The strings with characters.
+ */
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = (len / 2); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = ((len - 1) / 2); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
