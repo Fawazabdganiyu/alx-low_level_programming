894d8b49f7ab2bed66ce65a3751445cbf0f805b5
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-04-10 13:34:51 +0000

    Fix blank spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,11 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 1; j < 10; j++)
 		{
 			t = i * j;
 			if (t <= 9)
