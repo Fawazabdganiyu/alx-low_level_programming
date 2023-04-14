7d73e45766ff90dfed5c76eeda3562e429f6d8ad
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-04-14 20:46:51 +0000

    Fix code

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -37,7 +37,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (N_ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < new_size; i++)
+	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		N_ptr[i] = ptr_c[i];
 	}
