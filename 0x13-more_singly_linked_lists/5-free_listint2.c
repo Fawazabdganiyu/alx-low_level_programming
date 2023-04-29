2a1ab346d21af48d0628d3d4b4caba175cc05553
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-04-29 14:57:34 -0700

    Free list and st to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: The pointer to head pointer.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *temp2;
+
+	temp = *head;
+	while (temp)
+	{
+		temp2 = temp->next;
+		free(temp);
+		temp = temp2;
+	}
+	*head = NULL;
+}
