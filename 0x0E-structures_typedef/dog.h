cdab60d002645600441bdb26e6e472fee2d45a1b
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-04-29 00:36:30 -0700

    Define struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Struct name
+ * @name: The first member
+ * @age: The second member.
+ * @owner: The third member
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
