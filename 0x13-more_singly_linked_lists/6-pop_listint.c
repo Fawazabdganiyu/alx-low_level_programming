68253ee1c4291d2762717da5d6a57a3a9e1ddde8
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-04-30 11:51:43 -0700

    Free leaking memory

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,12 +11,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int n;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (0);
 
-	temp = *head;
-	*head = temp->next;
+	n = (*head)->n;
 
-	return (temp->n);
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return (n);
 }
