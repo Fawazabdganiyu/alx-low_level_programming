df4d85017eb3c9ddc1abd56e99fe53526748e354
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-05-01 21:04:48 +0000

    Fix betty style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 	current_index = 0;
-	while (current != NULL && current_index < idx -1)
+	while (current != NULL && current_index < idx - 1)
 	{
 		current = current->next;
 		current_index++;
