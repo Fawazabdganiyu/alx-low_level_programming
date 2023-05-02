9a51b4e490dd0b3dd1e77ea48eae785b8a4712d3
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-05-02 12:03:15 +0000

    Remove trailing whitespace

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	if (index == 0)
-	{	current = *head;	
+	{	current = *head;
 		*head = (*head)->next;
 		free(current);
 		return (1);
