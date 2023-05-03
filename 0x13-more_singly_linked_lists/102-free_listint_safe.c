9113c066730125eafdf30bb5331c15b081c00e18
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-05-02 21:45:34 -0700

    Add files

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: The pointer to head pointer.
+ *
+ * Return: The size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	return (0);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,14 @@
+#include"lists.h"
+
+/**
+ * find_listint_loop - find the loop in a linked list.
+ * @head: The head poionter.
+ *
+ * Return: The address of node where the loop start, or
+ * NULL, if there isno loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+
+{
+	return (0);
+}
