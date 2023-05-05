94eaaaa76953638f8b6281f9025c3b951e15744e
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-05-05 12:16:18 +0100

    Return bit at an index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,5 +8,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return (n >> index);
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	mask = 1UL << index;
+	if (n & mask)
+		return (1);
+	else
+		return (0);
 }
