d5587d45daf7ca1fc2b4e4952293513d1a493b80
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-05-05 11:31:35 +0100

    Print to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,5 +6,26 @@
  */
 void print_binary(unsigned long int n)
 {
-	_putchar(n & (1 << 11));
+	unsigned long int mask, size;
+	int leading_zeros = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	size = sizeof(n) * 8;
+	for (mask = (1UL << (size - 1)); mask > 0; mask >>= 1)
+	{
+		if (n & mask)
+		{
+			leading_zeros = 0;
+			_putchar('1');
+		}
+		else if (!leading_zeros)
+		{
+			_putchar('0');
+		}
+	}
 }
