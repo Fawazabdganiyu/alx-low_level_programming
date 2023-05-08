65735ab474bdb86786ef2580b67eb98e887d645d
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-05-08 11:46:09 -0700

    Improve code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int len, written;
 
-	if (filename == NULL || *filename == NULL)
+	if (filename == NULL || *filename == '\0')
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	for (len = 0; text_content[len]; len++)
-		;
+	;
 	if (text_content != NULL)
 	{
 		written = write(fd, text_content, len);
