3323621ef1a84ed13dc5b72756ca02f05c7c8bd4
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-05-08 21:53:28 -0700

    Add files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,41 +1,48 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file.
- * @filename: Name of the file to append.
- * @text_content: NULL terminated string to add at the end of the file.
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output.
+ * @filename: The name of file to read and print from.
+ * @letters: The number of letters it should read and print.
  *
- * Return: 1 on success, -1 on failure
- * if filename is NULL return -1
- * 1 if the file exists and -1 if the file does not exist
- * or if you do not have the required permissions to write the file.
+ * Return: The actual number of letters it could read and printed
+ * if the file can not be opened or read, return 0.
+ * If filename is NULL return 0
+ * If write fails or does not write the expected amount of bytes, return 0
  */
-int append_text_to_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int len, written;
+	int fd, nread, printed;
+	char *buffer;
 
-	if (filename == NULL || !filename)
-		return (-1);
+	if (filename == NULL)
+		return (0);
 
-	fd = open(filename, O_APPEND);
+	fd = 0;
+	nread = 0;
+	printed = 0;
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
+		return (0);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len]; len++)
-			;
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
+		return (0);
 
-		written = write(fd, text_content, len);
-		if (written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	nread = read(fd, buffer, letters);
+	if (nread == -1)
+		return (0);
+
+	printed = write(STDOUT_FILENO, buffer, nread);
+	if (printed == -1)
+		return (0);
+
+	free(buffer);
 
 	close(fd);
 
-	return (1);
+	return (printed);
 }
+
