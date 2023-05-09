b53836d63d03da744f609ff3f36f0931176f37ec
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-05-09 10:47:28 -0700

    Improve code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,27 +15,29 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int len, written;
 
-	if (filename == NULL || !filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (fd)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
-
-		written = write(fd, text_content, len);
-		if (written == -1)
+		if (text_content != NULL)
 		{
-			close(fd);
-			return (-1);
+			for (len = 0; text_content[len]; len++)
+				;
+
+			written = write(fd, text_content, len);
+			if (written == -1)
+			{
+				close(fd);
+				return (-1);
+			}
 		}
+		close(fd);
+		return (1);
 	}
-
-	close(fd);
-
-	return (1);
+	return (-1);
 }
