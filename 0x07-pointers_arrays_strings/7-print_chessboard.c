c463ba8117327440ceacec88ad22f1d54af08386
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-06-07 01:28:07 +0000

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: The array name'
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
+}
