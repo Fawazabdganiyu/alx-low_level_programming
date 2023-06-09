72e0a318b6b3d7c6e402d4a5e7d6f24256f938ac
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-06-09 03:23:29 +0000

    Improve code

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,19 +9,23 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+
 	/* Base condition */
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+
+	if (*s2 == '*')
+	{
+		if (*(s1 + 1) == *(s2 + 1))
+			return (wildcmp(s1 + 1, s2 + 1));
+		else
+			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
-	if (*s1 && *s2 == '*')
-	{
-		if ((*(s1 + 1) == *(s2 + 1)))
-			return (wildcmp(s1 + 1, s2 + 1));
-		else
-			return (wildcmp(s1 + 1, s2));
-	}
 	return (0);
 }
