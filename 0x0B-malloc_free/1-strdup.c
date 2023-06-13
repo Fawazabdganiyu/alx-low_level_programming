6a771496551ca91d13732aa87f0a27551e45c881
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-06-13 16:37:30 +0000

    Duplicate a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicate a string
+ * @str: string to be duplicated
+ *
+ * Return: a pointer to a new string which is a duplicate of the string str
+ *	NULL if str = NULL
+ *	NULL if insufficient memory was available
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int len, i;
+
+	for (len = 0; str[len]; len++)
+		;
+
+	s = malloc(sizeof(char) * (len + 1));
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
+}
