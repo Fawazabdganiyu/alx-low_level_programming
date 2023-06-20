676cb4888a567ca6612f486d1ed1c34b4c60d1b1
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-06-20 01:45:06 +0100

    Free alloc memory for dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees memories allocated for dogs
+ * @d: A pointer to dog_t type declaration name
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
