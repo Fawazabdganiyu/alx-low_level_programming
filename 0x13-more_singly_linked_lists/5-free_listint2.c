5faf04862363b3512cad98d08d371d098bcd57d4
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-07-05 11:48:30 +0100

    Free list and set to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: A pointer to the head pointer.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = *head;
+		free(temp);
+		*head = temp->next;
+	}
+	*head = NULL;
+}
