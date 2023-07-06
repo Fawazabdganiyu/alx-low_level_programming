930217b3f036dbf4ee83fa6d87f2621485c9cdf0
fawazabdganiyu <fawazabdganiyu@gmail.com>
2023-07-06 04:45:38 +0100

    Refactor code

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,27 +1,6 @@
 #include"lists.h"
 
 /**
- * find_loop - checks if the linked list has loop
- * @head: A pointer to the head node
- *
- * Return: A pointer to the meeting location in the search
- */
-listint_t *find_loop(listint_t *head)
-{
-	listint_t *fast = head, *slow = head;
-
-	while (fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return (fast);
-	}
-
-	return (NULL);
-}
-
-/**
  * find_listint_loop - find the loop in a linked list.
  * @head: The head poionter.
  *
@@ -30,23 +9,31 @@ listint_t *find_loop(listint_t *head)
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *fast, *slow;
+	listint_t *fast = head, *slow = head;
+
+	if (!head || !head->next)
+		return (NULL);
 
 	/* Check if the list has loop */
-	fast = find_loop(head);
-	if (fast)
+	while (fast && fast->next)
 	{
-		/* Set one of the (slow or fast) pointers back to the head */
-		slow = head;
-		/* Get the location of the loop */
-		while (slow != fast)
-		{
-			slow = slow->next;
-			fast = fast->next;
-		}
-
-		return (slow);
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			break;
 	}
 
-	return (NULL);
+	if (!fast || !fast->next)
+		return (NULL);
+
+	/* Set one of the (slow or fast) pointers back to the head */
+	slow = head;
+	/* Get the location of the loop */
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+
+	return (slow);
 }
