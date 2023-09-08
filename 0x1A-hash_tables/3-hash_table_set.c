86ff640db5d7826000d10e7a6b0771e114f1b517
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-08 21:39:59 +0100

    Add element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,86 @@
+#include "hash_tables.h"
+
+/**
+ * add_node - adds a node to the beginning of the linked list
+ * @head: A pointer to the head node pointer
+ * @key: The key, string
+ * @value: The value corresponding to a key
+ *
+ * Return: A pointer to a new node
+ */
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * search_list - searches the linked list for existing key
+ * @head: A pointer to the head node of the list.
+ * @key: The key, string
+ *
+ * Return: A pointer to the node where the key is found or NULL
+ */
+hash_node_t *search_list(hash_node_t *head, const char *key)
+{
+	while (head && (strcmp(head->key, key) != 0))
+		head = head->next;
+
+	if (head != NULL)
+		return (head);
+
+	return (NULL);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: The hash table to add or update the key/value to.
+ * @key: The key of the hash table.
+ * @value: The value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *head, *update_node;
+	char *temp;
+
+	if (strcmp(key, "") == 0)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/* Check if the index is NULL (Available for storage) */
+	head = ht->array[index];
+	if (head == NULL)
+	{	/* Set at this index */
+		add_node(&head, key, value);
+	}
+	else
+	{	/* Check the bucket */
+		update_node = search_list(head, key);
+		if (update_node != NULL)
+		{	/* Update the bucket */
+			temp = update_node->value;
+			update_node->value = strdup(value);
+			free(temp);
+		}
+		else /* Add it to the beginning of the list */
+			add_node(&head, key, value);
+	}
+
+	return (1);
+
+}
