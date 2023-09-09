7b54084d1329ac1a3e5150d368c2c0c04c4ca458
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-09 17:12:17 +0100

    Fix code

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,25 +15,22 @@ void hash_table_print(const hash_table_t *ht)
 
 	printf("{");
 
-	if (ht->array != NULL)
+	for (i = 0; i < ht->size; i++)
 	{
-		for (i = 0; i < ht->size; i++)
+		if (ht->array[i])
 		{
-			if (ht->array[i])
-			{
-				if (comma)
-					printf(", ");
+			if (comma)
+				printf(", ");
 
-				nodes = ht->array[i];
-				while (nodes)
-				{
-					printf("'%s': '%s'", nodes->key, nodes->value);
-					nodes = ht->array[i]->next;
-					if (nodes)
-						printf(", ");
-				}
-				comma = 1;
+			nodes = ht->array[i];
+			while (nodes)
+			{
+				printf("'%s': '%s'", nodes->key, nodes->value);
+				nodes = nodes->next;
+				if (nodes)
+					printf(", ");
 			}
+			comma = 1;
 		}
 	}
 	printf("}\n");
