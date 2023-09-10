d03fe8d9cbeb711b58b08988039cd6f477adfcc7
fawazabdganiyu <fawazabdganiyu@gmail>
2023-09-10 18:41:11 +0100

    Delete the sorted hash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -10,6 +10,9 @@ shash_node_t **create_array(unsigned long int size);
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 
 /**
  * add_node - adds a node to the beginning of the linked list
@@ -88,9 +91,7 @@ void insert_bucket(shash_table_t *ht, shash_node_t *new)
  */
 void sorted_insert(shash_table_t *ht, shash_node_t *new)
 {
-	shash_node_t *temp;
-	int i;
-	unsigned long int new_key, node_key;
+	shash_node_t *current;
 
 	if (ht->shead == NULL)
 	{
@@ -100,34 +101,35 @@ void sorted_insert(shash_table_t *ht, shash_node_t *new)
 		ht->stail = new;
 		return;
 	}
-	for (i = 0; new->key[i]; i++)
-		new_key += new->key[i]; /* Get the values of new key */
 
-	temp = ht->shead;
-	while (temp)
+	current = ht->shead;
+	while (current && strcmp(current->key, new->key) < 0)
+		current = current->snext;
+
+	if (current == ht->shead)
 	{
-		i = 0;
-		for (i = 0; temp->key[i]; i++)
-			node_key += temp->key[i];
-		if (node_key >= new_key)
-		{	/* Insert before this node */
-			new->snext = temp;
-			new->sprev = temp->sprev;
-			temp->sprev = new;
-			if (temp->sprev == NULL)
-				ht->shead = new;
-			return;
-		}
-
-		temp = temp->snext;
-	} /* Add it to the end */
-	temp = ht->shead;
-	while (temp && temp->snext)
-		temp = temp->snext;
-	new->snext = NULL;
-	new->sprev = temp;
-	temp->snext = new;
-	ht->stail = new;
+		/* Insert at the beginning */
+		new->sprev = current->sprev;
+		new->snext = current;
+		current->sprev = new;
+		ht->shead = new;
+	}
+	else if (current == NULL)
+	{
+		/* Insert at the end */
+		new->sprev = ht->stail;
+		new->snext = NULL;
+		ht->stail->snext = new;
+		ht->stail = new;
+	}
+	else
+	{
+		/* Insert in the middle */
+		new->sprev = current->sprev;
+		new->snext = current;
+		current->sprev->snext = new;
+		current->sprev = new;
+	}
 }
 /**
  * create_array - creates a pointer to an array of pointers
@@ -231,4 +233,73 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 		return (node->value);
 
 	return (NULL);
+}
+
+/**
+ * shash_table_print - prints the sorted hash table
+ * @ht: The sorted table to be printed.
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	node = ht->shead;
+	printf("{");
+	while (node)
+	{
+		printf("'%s': '%s'", node->key, node->value);
+		node = node->snext;
+		if (node)
+			printf(", ");
+	}
+	printf("}\n");
+
+}
+
+/**
+ * shash_table_print_rev - prints the sorted hash table in reverse order
+ * @ht: The sorted table to be printed in reverse.
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	node = ht->stail;
+	printf("{");
+	while (node)
+	{
+		printf("'%s': '%s'", node->key, node->value);
+		node = node->sprev;
+		if (node)
+			printf(", ");
+	}
+	printf("}\n");
+
+}
+
+/**
+ * shash_table_delete - deletes the sorted hash table
+ * @ht: The sorted hash table to be deleted
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+	shash_node_t *temp, *current;
+
+	current = ht->shead;
+	while (current)
+	{
+		temp = current->snext;
+		free(current->key);
+		free(current->value);
+		free(current);
+		current = temp;
+	}
+	free(ht->array);
+	free(ht);
 }
