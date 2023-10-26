0f3dd5e4263c7be0bf277c65a805bf78a9581f45
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-26 02:50:18 +0100

    Add file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module Name: 5-island_perimeter.py
+Description: This module provides a function `island_perimeter`
+"""
+
+
+def island_perimeter(grip):
+    """
+    Returns the perimeter of the island described in `grip`
+
+    * grid is a list of list of integers:
+        - 0 represents a water zone
+        - 1 represents a land zone
+        - One cell is a square with side length 1
+        - Grid cells are connected horizontally/vertically (not diagonally).
+        - Grid is rectangular, width and height don't exceed 100
+
+    * Grid is completely surrounded by water, and there is one island
+      (or nothing).
+    * The island doesn't have "lakes" (water inside that isn't connected
+      to the water around the island)
+
+    Args:
+        grip (list): A list of list of integers.
+    """
+    return 12
